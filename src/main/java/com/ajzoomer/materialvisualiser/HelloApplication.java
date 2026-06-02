eb6eda331762f2f45f6f3e875387a173872fde73
AJ Zoomer <alexjaiherrmann@gmail.com>
2026-06-02 20:33:21 +0800

    Initial commit for Material Visualiser project

---
+++ src/main/java/com/ajzoomer/materialvisualiser/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.ajzoomer.materialvisualiser;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Material Visualiser");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
